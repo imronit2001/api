b418ff13cdf0c0cfba6a9d7655bca515c1938675
Ronit Singh <ronitsingh7003@gmail.com>
2023-10-03 17:32:50 +0530

    #3 Fetching Records from API

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
+    /** Function to Create Student */
     public function createStudent(Request $request){
 
         $validator = Validator::make($request->all(),[
@@ -44,6 +45,199 @@ class StudentController extends Controller
                 "status" => "success",
                 "status_code" => 200,
                 "message" => "Student Created Successfully",
+                "data" => $student
+            ]);
+
+        }catch(Exception $e){
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Internal Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+
+    /** Function to Fetch All Students */
+    public function fetchStudents(){
+        try{
+            $student = Student::get();
+
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Students Fetched Successfully",
+                "data" => $student
+            ]);
+
+        }catch(Exception $e){
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Internal Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+
+    /** Function to fetchBCAStudents */
+    public function fetchBCAStudents(){
+        try{
+            $student = Student::where("stream","BCA")->get();
+
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "BCA Students Fetched Successfully",
+                "data" => $student
+            ]);
+
+        }catch(Exception $e){
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Internal Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+
+    /** Function to fetchStudentsByStream */
+    public function fetchStudentsByStream(Request $request){
+        try{
+
+            $student = [];
+
+            if($request->has("stream")){
+                $student = Student::where("stream",$request->stream)->get();
+            }else{
+                $student = Student::get();
+            }
+
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Students Fetched Successfully",
+                "data" => $student
+            ]);
+
+        }catch(Exception $e){
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Internal Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+
+    /**  Function to fetchStudentsByCharacter */
+    public function fetchStudentsByCharacter(Request $request){
+        try{
+
+            $student = [];
+            if($request->has("character")){
+                $student = Student::where("name","like",$request->character."%")->get();
+            }
+
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Students Fetched Successfully",
+                "data" => $student
+            ]);
+
+        }catch(Exception $e){
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Internal Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+
+    /** Function to fetchStudentDetails */
+    public function fetchStudentDetails(Request $request){
+        try{
+            $validator = Validator::make($request->all(),[
+                "id"=>"required"
+            ]);
+
+            if($validator->fails()){
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 400,
+                    "message" => "Validation Error",
+                    "errors" => $validator->errors()
+                ]);
+            }
+
+            $student = Student::where("id",$request->id)->first();
+
+            if($student){
+                return response()->json([
+                    "status" => "success",
+                    "status_code" => 200,
+                    "message" => "Student Fetched Successfully",
+                    "data" => $student
+                ]);
+            }else{
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 404,
+                    "message" => "Student Not Found",
+                    "data" => []
+                ]);
+            }
+
+
+        }catch(Exception $e){
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Internal Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+
+    /** Function to fetchStudentsInAscendingOrder */
+    public function fetchStudentsInAscendingOrder(){
+        try{
+            $student = Student::orderBy("name","asc")->get();
+
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Students Fetched Successfully",
+                "data" => $student
+            ]);
+
+        }catch(Exception $e){
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Internal Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+
+    /** Function to fetchStudentsAge */
+    public function fetchStudentsAge(){
+        try{
+
+            $student = Student::get();
+
+            foreach($student as $s){
+                $s->age = date_diff(date_create($s->dob),date_create(date("Y-m-d")))->y;
+            }
+
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Students Age Fetched Successfully",
                 "data" => $student
             ]);
 
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,24 @@ use Illuminate\Support\Facades\Route;
 
 /** Route to Create Student */
 Route::post("/student/create",[StudentController::class,"createStudent"]);
+
+/** Route to Fetch All Student's Data */
+Route::get("/student/fetch",[StudentController::class,"fetchStudents"]);
+
+/** Route to Fetch All Students who study in BCA */
+Route::get("/student/fetch/bca",[StudentController::class,"fetchBCAStudents"]);
+
+/** Route to Fetch All Student according to their stream */
+Route::get("/student/fetch/stream",[StudentController::class,"fetchStudentsByStream"]);
+
+/** Route to Fetch Students whose name starts with a character */
+Route::get("/student/fetch/character",[StudentController::class,"fetchStudentsByCharacter"]);
+
+/** Route to Fetch Student by Id */
+Route::get("/student/details",[StudentController::class,"fetchStudentDetails"]);
+
+/** Route to Fetch student in ascending order or their name */
+Route::get("/student/fetch/ascending",[StudentController::class,"fetchStudentsInAscendingOrder"]);
+
+/** Route to Fetch All Student Age */
+Route::get("/student/fetch/age",[StudentController::class,"fetchStudentsAge"]);
